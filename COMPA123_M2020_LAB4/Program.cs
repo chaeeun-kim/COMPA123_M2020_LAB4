c9c00e93f4dd847f66af6260380664b14de94223
김채은 <afafpo@naver.com>
2020-06-18 13:05:56 +0900

    초기 커밋.

---
+++ COMPA123_M2020_LAB4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace COMPA123_M2020_LAB4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
